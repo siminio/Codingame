a288b37f1877abbe077acd40f863d7409b6aa64c
unknown <14363367+siminio@users.noreply.github.com>
2018-07-01 11:40:36 +0200

    Modification of the temperatures solution

--- easy/Temperatures.hs
+++ easy/Temperatures.hs
@@ -1,28 +1,19 @@
 import System.IO
 import Control.Monad
+import Data.List
+import Data.Ord
 
-giveNearestTempFromZero :: [Int] -> [Int] -> String
-giveNearestTempFromZero [] [] = "0"
-giveNearestTempFromZero tempPos [] = "" ++ show (minimum tempPos)
-giveNearestTempFromZero [] tempNeg = "-" ++ show (minimum (map abs tempNeg))
-giveNearestTempFromZero tempPos tempNeg
-    | minNeg < minPos = "-" ++ show (abs minNeg)
-    | otherwise = show minPos
-    where minNeg = minimum (map abs tempNeg)
-          minPos = minimum tempPos
+giveClosestTempFromZero :: [Int] -> Int
+giveClosestTempFromZero [] = 0
+giveClosestTempFromZero temps = minimumBy (comparing cmp) temps
 
+cmp x = (x^2) - x
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-    
     input_line <- getLine
     let n = read input_line :: Int -- the number of temperatures to analyse
     input_line <- getLine
     let input = words input_line
     let temps = map read input :: [Int]
-    let tempPos = filter (>=0) temps
-    let tempNeg = filter (<0) temps
-    putStrLn $ giveNearestTempFromZero tempPos tempNeg
-
-    return ()
+    putStrLn $ show $ giveClosestTempFromZero temps
